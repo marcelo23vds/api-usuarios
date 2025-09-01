6350c8b44156982951df9f0f69b36d7535e94d1c
MARCELO VIEIRA DA SILVA <25132416@CFP127ALUNO.LOCAL>
2025-09-01 10:15:28 -0300

    melhorando o codigo

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -11,32 +11,27 @@ const userModel = require('../model/userModel')
 const createUser = (req, res) => {
     const {name, email} = req.body
 
-    if (!name || !email) {
-        return res.status(400).json({message: 'Nome e email são obrigatorios!'})
-    } else {
-        const newUser = userModel.create({name, email})
-        //codigo de status 201 significa que foi criado com sucesso
-        res.status(201).json(newUser) 
-    }
+    const newUser = userModel.create({name, email})
+    //codigo de status 201 significa que foi criado com sucesso
+    res.status(201).json(newUser) 
 }
 
 // controlador para buscar um usuário por ID
 const getUserById = (req, res) => {
+    
     // extrai o parametro id
     const id = req.params.id
     const user = userModel.findById(id)
 
-    if (!user) {
-        return res.status(404).json({ message: 'Usuário não encontrado!' })
-    } else {
-        res.status(200).json(user)
-    }
+    res.status(200).json(user)
+
 }
 
 
 // controlador para listar todos os usuarios
 const getAllUsers = (req, res) => {
     const users = userModel.findAll()
+
     res.status(200).json(users)
 }
 
